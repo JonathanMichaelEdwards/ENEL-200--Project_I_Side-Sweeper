0614259ccefc8f389c1fe17e2d655fa867835ef2
Jonathan <JonathanMichaelEdwards>
2019-09-10 07:06:23 +1200

    app dev

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
 #include <SparkFun_TB6612.h>
 
-// Defines the physical locations of the pins
+// Pins from the arduino
+#define HORN 12
+#define TRIG_PIN 11
+#define PULSE_PIN 10
 #define PWMA 9
 #define AIN2 8
 #define AIN1 7
@@ -10,23 +13,26 @@
 #define BIN2 4
 #define PWMB 3
 
+// changes the motor direction (-1 reverses motor)
+#define OFFSet_A 1
+#define OFFSet_B 1
 
-#define TRIG_PIN 11
-#define PULSE_PIN 10
+#define HORN_DELAY 500
 #define TRIG_DELAY 10
 #define TIME_CONVERSION 58.8
+#define SPEED_RATE 25.5
 
-// Use this to change motor direction (-1 reverses motor)
-const int offsetA = 1;
-const int offsetB = 1;
 
 // Initialise motors
-Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
-Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
+Motor motor1 = Motor(AIN1, AIN2, PWMA, OFFSet_A, STBY);
+Motor motor2 = Motor(BIN1, BIN2, PWMB, OFFSet_B, STBY);
 
-// Code to detect distance to obstacle
-double measureDistance() {
-  
+
+/*
+ * detect distance to obstacle
+ */
+double measureDistance(void) 
+{
   long duration = 0;
  
   // Tell distance sensor to send a pulse
@@ -42,53 +48,63 @@ double measureDistance() {
   duration = pulseIn(PULSE_PIN, HIGH);
  
   // Convert time to distance
-  return (uint8_t)(duration / TIME_CONVERSION);
+  return (duration / TIME_CONVERSION);
 }
 
 
-void setup()
+void horn(void)
+{
+    digitalWrite(HORN, HIGH);
+    delay(HORN_DELAY);
+    digitalWrite(HORN, LOW);
+    delay(HORN_DELAY);
+}
+
+
+void reverseSensor(void)
+{
+    brake(motor1, motor2);
+    horn();
+    horn();
+    back(motor1, motor2, 200);
+    horn();
+    horn();
+    horn();
+    brake(motor1, motor2);
+}
+
+
+void control(double dist, char command, uint8_t speed)
+{
+    if (5 < dist && dist < 20) return;
+    else if(command =='F') forward(motor1, motor2, speed); 
+    else if(command =='B') back(motor1, motor2, speed);
+    else if(command =='L') left(motor1, motor2, speed);
+    else if(command =='R') right(motor1, motor2, speed);
+    else if(command =='S') brake(motor1, motor2);
+    else if(command =='V') horn();
+}
+
+
+void setup(void)
 {
   Serial.begin(9600);
-  pinMode(12, 1);
-  digitalWrite(12, 0);
+  pinMode(HORN, OUTPUT);
 }
+ 
 
-
-void beeper()
+void loop(void)
 {
-    digitalWrite(12, 1);
-    delay(500);
-    digitalWrite(12, 0);
-    delay(500);
-}
-
-
-void loop()
-{
-  // beeper();
-    // forward(motor1, motor2, 220);
+    uint8_t speed = 0;
     double dist = measureDistance();
-
     char command = Serial.read();
+    
+    // Determine the throttle
+    if (0 < command && command <= 9) speed = (command*SPEED_RATE);  
 
-    for (char option = 'a'; option < 'z'; option++) {
-        if (command == option) {
-            if (5 < dist && dist < 15) break;
-            else if(command =='f') forward(motor1, motor2, 255); 
-            else if(command =='b') back(motor1, motor2, 255);
-            else if(command =='l') right(motor1, motor2, 255);
-            else if(command =='r') left(motor1, motor2, 255);
-            else if(command =='s') brake(motor1, motor2);
-            else if(command =='p') beeper();
-        } 
-    }
+    // Changing the speeds
+    if (command < 'A' && command <= 'Z') control(dist, command, speed);
 
-    if (5 < dist && dist < 15) {
-        brake(motor1, motor2);
-        beeper();
-        back(motor1, motor2, 255);
-        beeper();
-        beeper();
-        brake(motor1, motor2);
-    } 
+    // Sensor's limit
+    if (5 < dist && dist < 20) reverseSensor(); 
 }
